9a6998c77a96776dd4041949a8eb8bc27b128e4f
Vladpers1k <vladik340123@gmail.com>
2024-12-20 16:10:15 +0200

    end

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,17 @@
+import React from 'react'
+import Button from './components/button'
+
 function App() {
-  return <h1>Hello_React</h1>
+  const handleClick = () => {
+    alert('Button clicked!')
+  }
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>React Button</h1>
+      <Button text="Click Me!" onClick={handleClick} />
+    </div>
+  )
 }
 
 export default App
